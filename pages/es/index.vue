6c228db5827d6c08e10451b7f8dd864d769d5895
andres <andresclua@gmail.com>
2021-11-25 11:25:50 +0100

    pages

---
+++ pages/es/index.vue
@@ -0,0 +1,41 @@
+<template>
+  <div>
+
+    <h2>hey undp! this might be in Spanish</h2>
+    <p>this is working in github?</p>
+
+  </div>
+</template>
+
+<script>
+/* Components */
+
+
+/* Queries */
+import { groq } from '@nuxtjs/sanity';
+
+
+export default {
+  head() {
+      return {
+        title: this.title,
+        meta: [
+          // hid is used as unique identifier. Do not use `vmid` for it as it will not work
+          {
+            hid: 'description',
+            name: 'description',
+            content: 'UNDP undp-year-ahead resolution'
+          }
+        ]
+      }
+  },
+  data: () => ({
+     title: 'Title undp-year-ahead ES' 
+  }),
+
+  
+}
+</script>
+<style lang="scss">
+  @import '@/sass/style.scss';
+</style>
