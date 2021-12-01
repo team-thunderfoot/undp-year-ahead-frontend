14bc3c0ae8f7ef6d9d651423dc5dc084e5ce3c28
Nerea Fontecha <nerea@landt.co>
2021-12-01 16:05:16 +0100

    progess

--- components/ProgressNav.vue
+++ components/ProgressNav.vue
@@ -3,19 +3,19 @@
         <ul class="b--progress-a__wrapper">
             <li class="b--progress-a__wrapper__item" ref="first" @click.prevent="goTo('chapter1')">
                 <svg class="b--progress-a__wrapper__item__icon"><use xlink:href="#checkmark-bold"/></svg>
-                <span class="b--progress-a__wrapper__item__label">Cheese</span>
+                <span class="b--progress-a__wrapper__item__label"></span>
             </li>
             <li class="b--progress-a__wrapper__item" ref="second" @click.prevent="goTo('chapter2')">
                 <svg class="b--progress-a__wrapper__item__icon"><use xlink:href="#checkmark-bold"/></svg>
-                <span class="b--progress-a__wrapper__item__label">Pizza</span>
+                <span class="b--progress-a__wrapper__item__label"></span>
             </li>
-            <li class="b--progress-a__wrapper__item" ref="third" @click.prevent="goTo('chapter1')">
+            <li class="b--progress-a__wrapper__item" ref="third" @click.prevent="goTo('chapter3')">
                 <svg class="b--progress-a__wrapper__item__icon"><use xlink:href="#checkmark-bold"/></svg>
-                <span class="b--progress-a__wrapper__item__label">Steak</span>
+                <span class="b--progress-a__wrapper__item__label"></span>
             </li>
-            <li class="b--progress-a__wrapper__item" ref="fourth" @click.prevent="goTo('chapter2')">
+            <li class="b--progress-a__wrapper__item" ref="fourth" @click.prevent="goTo('chapter4')">
                 <svg class="b--progress-a__wrapper__item__icon"><use xlink:href="#checkmark-bold"/></svg>
-                <span class="b--progress-a__wrapper__item__label">Potatoes</span>
+                <span class="b--progress-a__wrapper__item__label"></span>
             </li>
         </ul>
     </div>
--- pages/index.vue
+++ pages/index.vue
@@ -40,6 +40,35 @@
                 </div>
             </div>
             
+			<div class="b--page-a__wrapper__item b--ss-a" id="Scene3"> 
+                <div class="b--ss-a__ft-items">
+                    <div class="b--bird-a" v-rellax="rellax">
+                        <img src="@/assets/img/bird.png" alt="">
+                    </div>
+                    <img class="b--ss-a__ft-items__artwork" src="@/assets/img/ft-chapter-1.png" alt=""> 
+                </div>
+                <div class="b--ss-a__content">
+                    <div class="b--card-a">
+                        <div class="b--card-a__hd">
+                            <h3 class="b--card-a__hd__title">
+                                Towards living in harmony with nature
+                            </h3> 
+                        </div>
+                
+                    </div>
+                </div>
+                <div class="b--ss-a__bg-items">
+                    <img class="b--ss-a__bg-items__artwork" src="@/assets/img/bg-chapter-1.png" alt="">    
+                </div>
+            </div>
+			<div class="b--page-a__wrapper__item b--page-a__wrapper__item--second" id="Scene4">
+                <div class="b--ss-a__ft-items">
+                    <img class="b--ss-a__ft-items__artwork" src="@/assets/img/ft-chapter-2.png" alt=""> 
+                </div>
+                <div class="b--ss-a__bg-items">
+                    <img class="b--ss-a__bg-items__artwork" src="@/assets/img/bg-chapter-2.png" alt="">    
+                </div>
+            </div>
         </div>
 		{refValue}}
 		<v-progress-nav :refValue="refValue" ref="processA"  @goToChapter="goToChapter"/>
@@ -92,7 +121,25 @@ export default {
 						}
 						//ya estoy probando de todo, asi que no asustarse
 						break;
-				
+					case '#chapter3':
+						// Get the element
+						let scene3  = document.querySelector("#Scene3");
+						if(scene3){
+							var pos =  scene3.offsetLeft;
+							this.$gsap.to(window, {duration: 2, scrollTo: pos }); 
+						}
+						
+						break;
+
+					case '#chapter4':
+						// Get the element
+						let scene4  = document.querySelector("#Scene4");
+						if(scene4){
+							var pos =  scene4.offsetLeft;
+							this.$gsap.to(window, {duration: 2, scrollTo: pos }); 
+						}
+						
+						break;
 					default:
 						let sceneDefault  = document.querySelector("#Scene2");
 						if(sceneDefault){
