47abe27822a10377bca146c4b92a92abc816c9aa
Nerea Fontecha <nerea@landt.co>
2021-12-10 17:09:22 +0100

    fixed nav

--- pages/es/index.vue
+++ pages/es/index.vue
@@ -2,7 +2,6 @@
     <div>
         <div class="b--noise-a"></div>
         <v-nav />
-        <v-lang />
         <v-lang v-if="isLoaded" />
         <div class="b--preloader-a" :class="{'b--preloader-a--is-hidden':isLoaded}">
             <div class="b--preloader-a__wrapper">
