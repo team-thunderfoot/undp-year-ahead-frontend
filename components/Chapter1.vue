3fdc11aa493e00584ca92e6ab0f504fe23c89c77
andres <andresclua@gmail.com>
2021-12-10 11:23:15 -0500

    changes lazy load

--- components/Chapter1.vue
+++ components/Chapter1.vue
@@ -71,7 +71,7 @@
                 </div>
 
                 <div class="b--chapter1-a__artwork b--chapter1-a__artwork--fifth">
-                   <div class="b--motion-a">
+                   <div class="b--motion-a" v-lazy:background-image="require(`@/assets/img/chapter-1/bubble-motion.png`)">
                         <!-- bubble -->
                     </div>
                 </div>
--- plugins/vue-lazyload.js
+++ plugins/vue-lazyload.js
@@ -3,6 +3,12 @@ import VueLazyload from "vue-lazyload"
 
 Vue.use(VueLazyload, {
     preLoad: 1.3,
+    observer: true,
+    // lazyComponent: true,
+    observerOptions: {
+        rootMargin: '-300px',
+        threshold: 0.1
+    }
     // attempt: 1,
     // loading: '/img/placeholder.svg'
 });
