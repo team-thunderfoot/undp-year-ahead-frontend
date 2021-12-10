037f85eb267d9d3070a6ef04fa7cdf8a93125a4c
Nerea Fontecha <nerea@landt.co>
2021-12-10 10:38:33 +0100

    info window

--- components/Chapter1.vue
+++ components/Chapter1.vue
@@ -89,7 +89,7 @@
                             i
                         </div>
                     </div>
-                    <div class="b--info-window-a" :class="{ 'b--info-window-a--is-visible': this.infoWindowStatus}">
+                    <div class="b--info-window-a" :class="{ 'b--info-window-a--is-visible': this.infoWindowStatus}" @click.prevent="this.infoWindowStatus == !this.infoWindowStatus">
                         <div class="b--info-window-a__content">
                             <SanityContent :blocks="chapter.info"  />
                         </div>
@@ -121,6 +121,7 @@ export default {
             totalContent:3,
 			contentLoaded : 0,
             chapter : null,
+            infoWindowStatus: false
 		}
 	},
     components:{
