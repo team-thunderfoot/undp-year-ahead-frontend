93da2b9e5f0ff8d7cd3183eb1ac606e5efdddfb7
Gonzalo Alcina <gonzalo@teamthunderfoot.com>
2021-12-14 17:27:52 +0100

    Clean html

--- components/Chapter4.vue
+++ components/Chapter4.vue
@@ -11,23 +11,6 @@
                 />
             </div>
             <div class="b--ss-a__content">
-                <!-- <div class="b--card-a">
-                    <div class="b--card-a__hd">
-                        <h3 class="b--card-a__hd__title">
-                            {{chapter.title}}
-                        </h3> 
-                    </div>
-                    <div class="b--card-a__bd">
-                        <p class="b--card-a__bd__content">
-                            <SanityContent :blocks="chapter.description" />
-                        </p>
-                    </div>
-                </div>
-                <div class="b--card-b">
-                    <p class="b--card-b__content">
-                        <SanityContent :blocks="chapter.content" />
-                    </p>
-                </div> -->
                 <!-- first position element, card -->
                 <div class="b--chapter4-a__content">
                     <v-card-f 
@@ -41,13 +24,11 @@
                     <div class="b--quote-a b--quote-a--second">
                         <div class="b--quote-a__content">
                             {{ chapter.quote }}
-                            <!-- “In a world where complex and multidimensional crisis has become the norm, the status quo for crisis response no longer an option.”   -->
                         </div>
                         <div class="b--quote-a__meta">
                         <h3 class="b--quote-a__meta__title">{{ chapter.quotetitle }}</h3>
                         <h4 class="b--quote-a__meta__subtitle">
                             {{ chapter.quotedirector }} 
-                            <!-- <u>Asako Okai</u>, UNDP Crisis Bureau Director -->
                         </h4>
                         </div>
                     </div>
