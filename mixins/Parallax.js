e338a9db94cedf5f3865dd7f74e60e734731cb9c
andres <andresclua@gmail.com>
2021-12-16 23:22:32 +0100

    no message

---
+++ mixins/Parallax.js
@@ -0,0 +1,25 @@
+export default {
+    methods:{
+        parallaxMove({
+            containerAnimation,
+            element,
+            scrub,
+            intensity
+        }){
+            
+            let scene1 = document.querySelector("#Scene1");
+            this.$gsap.to(element, {
+                x: () =>  scene1.offsetWidth * intensity,
+                ease: "none",
+                scrollTrigger: {
+                    containerAnimation: containerAnimation,
+                    scrub: scrub,
+                    markers: false,
+                    id: "1",
+                    invalidateOnRefresh: true,
+                }
+            });
+
+        }
+    },
+}
