e001c2f085874a772837c4c60b799d2172dde6f6
Irati Antía <irati@longneckandthunderfoot.com>
2021-12-21 13:02:53 +0100

    image update chapter-4

--- components/Chapter4.vue
+++ components/Chapter4.vue
@@ -100,8 +100,8 @@ export default {
         scrollTween(newValue, oldValue){
             if (newValue ) {
                 var motion = [
-                    {obj:this.$refs['parallax-bg'], intensity:-3},
-                    {obj:this.$refs['parallax-ft'], intensity:-5},
+                    {obj:this.$refs['parallax-bg'], intensity:-1},
+                    {obj:this.$refs['parallax-ft'], intensity:-3},
                 ];
                 motion.forEach(item => {
                     this.parallaxMove({
