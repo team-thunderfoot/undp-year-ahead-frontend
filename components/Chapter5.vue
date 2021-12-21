6ee2d742bc1395c55803f06227a6ff0bb632e886
andres <andresclua@gmail.com>
2021-12-21 19:21:44 +0100

    chapter 5 motion

--- components/Chapter5.vue
+++ components/Chapter5.vue
@@ -2,11 +2,11 @@
   <section class="b--page-a__item b--chapter5-a" id="Scene5" ref="Scene5" v-if="chapter">
     <div class="b--ss-a">
       <div class="b--ss-a__ft-items">
-        <img class="b--ss-a__ft-items__parallax" ref="parallax-ft" src="@/assets/img/chapter-5/front.png">
+        <img class="b--ss-a__ft-items__parallax" ref="parallax-ft" :style="{left: '30%' }" src="@/assets/img/chapter-5/front.png">
       </div>
       <div class="b--ss-a__content">
         <!-- chapter title -->
-        <div class="b--chapter5-a__content">
+        <div class="b--chapter5-a__content" :style="{left: '35%' }" ref="boxContent">
             <v-card-f 
                 :title="chapter.title"
                 :description="chapter.description"
@@ -15,7 +15,7 @@
             />
         </div>
         <!-- info chart -->
-        <div class="b--chapter5-a__content b--chapter5-a__content--second">
+        <div class="b--chapter5-a__content b--chapter5-a__content--second" ref="infochapter" :style="{left: '60%' }">
            <v-info-chapter :info="chapter"/>
         </div>
         <!-- women -->
@@ -42,7 +42,7 @@
         </div>    
       </div>
       <div class="b--ss-a__bg-items">
-        <img class="b--ss-a__bg-items__parallax" ref="parallax-bg" src="@/assets/img/chapter-5/back-parallax.png" alt=""> 
+        <img class="b--ss-a__bg-items__parallax" :style="{left: '2%' }" ref="parallax-bg" src="@/assets/img/chapter-5/back-parallax.png" alt=""> 
         <img class="b--ss-a__bg-items__back" @load="handleLoad"  @error="handleLoad" src="@/assets/img/chapter-5/back.png"> 
       </div>
     </div>
@@ -54,11 +54,12 @@ import { groq } from '@nuxtjs/sanity';
 import CardF from '@/components/cards/CardF';
 import InfoChapter from '@/components/infochapter/Infochapter';
 
-// import Parallax from '@/motion/Parallax';
+
 import Vue from 'vue';
 import Parallax from '@/mixins/Parallax.js';
 import Animation from '@/mixins/Animation.js';
 Vue.use(Parallax)
+Vue.use(Animation)
 
 export default {
   mixins: [Parallax,Animation],
@@ -91,14 +92,25 @@ export default {
     handleLoad() {
       this.contentLoaded++
     },
-    animate() {
-      this.$nextTick(() => {
-        this.startAnimation({
-            sceneID : 5,
-            scrub:0,
-            scrollTween : this.scrollTween
-        })
-      })
+    AsambleParallaxObjs(){
+        var motion = [
+            {obj:this.$refs['parallax-bg'], intensity:4},
+            {obj:this.$refs['parallax-ft'], intensity:11},
+            {obj:this.$refs['infochapter'], intensity:11},
+            {obj:this.$refs['boxContent'], intensity:11},
+            
+            // {obj:this.$refs['boxContent'], intensity:11},
+            // {obj:this.$refs['quoteContent'], intensity:4},
+        ];
+        motion.forEach(item => {
+            this.parallaxMove({
+                el: item.obj,
+                intensity:item.intensity,
+                duration: this.$refs['Scene5'].offsetWidth,
+                containerAnimation:this.scrollTween,
+                scrub:1,
+            })  
+        });
     },
   },
   watch: {
@@ -110,7 +122,14 @@ export default {
     },
         scrollTween(newValue, oldValue){
             if (newValue ) {
-                this.animate();
+               // motion frontend and backend elements
+                this.AsambleParallaxObjs();
+                // mixin function
+                this.startAnimation({
+                    sceneID : 5,
+                    scrub:0,
+                    scrollTween : this.scrollTween
+                })
             } 
         }
   },
