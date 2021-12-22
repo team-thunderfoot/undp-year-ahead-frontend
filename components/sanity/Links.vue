669e3958b619dbc49b178bb1a5201a6ff5870299
Nerea Fontecha <nerea@landt.co>
2021-12-22 15:22:22 +0100

    vbar

--- components/sanity/Links.vue
+++ components/sanity/Links.vue
@@ -1,6 +1,6 @@
 <template functional>
     <div>
-        <a v-if="props.newtab" :href="props.href" target="blank_" rel="nofollow noopener"><slot /></a>
+        <a v-if="props.blank" :href="props.href" target="blank_" rel="nofollow noopener"><slot /></a>
         <a v-else :href="props.href" rel="nofollow noopener"><slot /></a>
     </div>
 </template>
