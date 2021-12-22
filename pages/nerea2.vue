897b337ed624d02bfbf151d74cdf3477ad944014
Nerea Fontecha <nerea@landt.co>
2021-12-22 13:57:04 +0100

    nerea2

--- pages/nerea2.vue
+++ pages/nerea2.vue
@@ -7,18 +7,3 @@
         <!-- your content -->
     </v-bar>
 </template>
-<script>
-import { PerfectScrollbar } from 'vue2-perfect-scrollbar'
-
-export default {
-    components: {
-        PerfectScrollbar
-    }
-}
-</script>
-<style src="vue2-perfect-scrollbar/dist/vue2-perfect-scrollbar.css"/>
-/* example */
-.ps {
-  height: 400px;
-}
-</style>
