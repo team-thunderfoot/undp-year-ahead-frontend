7e0f9fa755afe73447731db787741606bf1dd3dc
andres <andresclua@gmail.com>
2021-12-28 16:18:02 +0100

    update 12

--- components/Chapter12.vue
+++ components/Chapter12.vue
@@ -3,20 +3,21 @@
         <div class="b--ss-a"> 
             <div class="b--ss-a__divider">
                 <div class="b--chapter12-a__divider">
-                <img
-                    class="b--chapter12-a__divider__media b--chapter12-a__divider__media--left"
-                    v-lazy="require(`@/assets/img/chapter-12/11-12_tree.png`)"
-                />
+                    <img
+                        class="b--chapter12-a__divider__media b--chapter12-a__divider__media--left"
+                        v-lazy="require(`@/assets/img/chapter-12/11-12_tree.png`)"
+                    />
                 </div>
             </div>
             <div class="b--ss-a__ft-items">
-                <img class="b--ss-a__ft-items__parallax" v-lazy="require(`@/assets/img/chapter-12/front-parallax.png`)" alt="front" />
+                <img ref="parallax-ft" :style="{ left: '490%' }" class="b--ss-a__ft-items__parallax" v-lazy="require(`@/assets/img/chapter-12/front-parallax.png`)" alt="front" />
             </div>
             <div class="b--ss-a__content">
                 <!-- chapter title -->
                 <div 
                     class="b--chapter12-a__content"
                     :class="'b--chapter12-a__content--' + `${this.lang}`"
+                    ref="boxContent"  :style="{ left: '508%' }"
                 >
                     <v-card-f 
                         :title="chapter.intro_title"
@@ -26,7 +27,9 @@
                     />
                 </div>
                 <!-- info chart -->
-                <div class="b--chapter12-a__content b--chapter12-a__content--second">
+                <div class="b--chapter12-a__content b--chapter12-a__content--second"
+                 ref="infoChapter"  :style="{ left: '530%' }"
+                >
                     <v-info-chapter :info="chapter"/>
                 </div>
                 <div class="b--chapter12-a__artwork">
@@ -39,6 +42,7 @@
             <div class="b--ss-a__bg-items">
                 <img class="b--ss-a__bg-items__parallax"
                 alt="back-parallax"
+                ref="parallax-bg'"
                 v-lazy="require(`@/assets/img/chapter-12/back-parallax.png`)"
                 >        
                 <img class="b--ss-a__bg-items__back" @load="handleLoad"  @error="handleLoad" :src="require(`@/assets/img/chapter-12/back.png`)" alt="back">        
@@ -72,15 +76,23 @@ export default {
         handleLoad(){
             this.contentLoaded++;
         },
-        animate(){
-            this.$nextTick(() => {
-                this.startAnimation({
-                    sceneID : 12,
-                    scrub:0,
-                    scrollTween : this.scrollTween
-                })
-            })
-        }
+        AsambleParallaxObjs() {
+      var motion = [
+        { obj: this.$refs['parallax-bg'], intensity: 1 },
+        { obj: this.$refs['parallax-ft'], intensity: 55 },
+        { obj: this.$refs['infoChapter'], intensity: 55 },
+        { obj: this.$refs['boxContent'], intensity: 55 },
+      ]
+      motion.forEach((item) => {
+        this.parallaxMove({
+          el: item.obj,
+          intensity: item.intensity,
+          duration: this.$refs['Scene12'].offsetWidth,
+          containerAnimation: this.scrollTween,
+          scrub: true,
+        })
+      })
+    },
     },
     watch: {
         contentLoaded(newValue, oldValue) {
@@ -91,7 +103,14 @@ export default {
         },
         scrollTween(newValue, oldValue){
             if (newValue ) {
-                this.animate();
+                //motion frontend and backend elements
+                this.AsambleParallaxObjs()
+                // mixin function
+                this.startAnimation({
+                    sceneID : 12,
+                    scrub:0,
+                    scrollTween : this.scrollTween
+                })
             } 
         }
     },
