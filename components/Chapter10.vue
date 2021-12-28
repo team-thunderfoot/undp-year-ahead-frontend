d16fa776dfaac3b701642aaac6cc8ef9cfd829cd
andres <andresclua@gmail.com>
2021-12-28 14:58:30 +0100

    update chapter 10

--- components/Chapter10.vue
+++ components/Chapter10.vue
@@ -31,7 +31,7 @@
         />
       </div>
       <!-- animation out of content, fisherman and plant needs more z-index than ft-items and ft-items needs more z-index than content -->
-      <div class="b--chapter10-a__artwork" ref="fisherman"  :style="{ left: '328%' }">
+      <div class="b--chapter10-a__artwork" ref="fisherman"  :style="{ left: '336%' }">
         <div
           class="b--motion-f"
           v-lazy:background-image="
@@ -52,7 +52,7 @@
         <div
           class="b--chapter10-a__content"
           :class="'b--chapter10-a__content--' + `${this.lang}`"
-          ref="boxContent"  :style="{ left: '305%' }"
+          ref="boxContent"  :style="{ left: '298%' }"
         >
           <v-card-f
             :title="chapter.intro_title"
