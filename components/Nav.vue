7aa268e84a519221a72364bad28cf8a4b9108e1a
Nerea Fontecha <nerea@landt.co>
2021-12-28 10:00:12 +0100

    Insgram icon

--- components/Nav.vue
+++ components/Nav.vue
@@ -13,7 +13,7 @@
                         <img class="b--nav-a__ft-items__ft__link__media" src="@/assets/img/social/facebook-black.svg" alt="Facebook">
                     </a>
                     <a class="b--nav-a__ft-items__ft__link" :href="social.instagram" target="_blank" rel="noopener noreferrer">
-                        <img class="b--nav-a__ft-items__ft__link__media" src="@/assets/img/social/twitter-black.svg" alt="Instagram">
+                        <img class="b--nav-a__ft-items__ft__link__media" src="@/assets/img/social/instagram-black.svg" alt="Instagram">
                     </a>
                     <a class="b--nav-a__ft-items__ft__link" :href="social.twitter" target="_blank" rel="noopener noreferrer">
                         <img class="b--nav-a__ft-items__ft__link__media" src="@/assets/img/social/twitter-black.svg" alt="Twitter">
