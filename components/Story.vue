7c728fd792ccbf5fbee060361d06fc8396ab54b6
Nerea Fontecha <nerea@landt.co>
2021-12-29 14:16:51 +0100

    refresh

--- components/Story.vue
+++ components/Story.vue
@@ -164,7 +164,7 @@ export default {
                 }
             });
             
-            window.onresize = ()=> { location.reload(); }
+            // window.onresize = ()=> { location.reload(); }
         }
     }
 }
