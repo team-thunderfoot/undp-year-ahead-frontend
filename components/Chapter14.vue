0542657401f37a049be344ce124c8e35a60438ca
Nerea Fontecha <nerea@landt.co>
2021-12-29 14:40:58 +0100

    delete vlazy

--- components/Chapter14.vue
+++ components/Chapter14.vue
@@ -25,7 +25,7 @@
                 </div>
                 <!-- arrow -->
                 <div class="b--chapter14-a__artwork">
-                    <div class="b--motion-o" v-lazy:background-image="require(`@/assets/test_sprites/arrow.png`)"></div>
+                    <div class="b--motion-o" :style="'background-image: url(' + require(`@/assets/test_sprites/arrow.png`) + ')'"></div>
                 </div>
                 <!-- wheel -->
                 <div class="b--chapter14-a__artwork b--chapter14-a__artwork--second">
