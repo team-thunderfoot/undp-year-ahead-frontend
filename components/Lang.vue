96b79e6fdd2197137b26b04e627b766e2c630ede
Irati Antía <irati@longneckandthunderfoot.com>
2022-01-03 13:52:01 +0100

    lang

--- components/Lang.vue
+++ components/Lang.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="b--lang-a" data-dropdown-languages v-if="languages" :class="{'b--lang-a--is-hidden' : !langIsVisible}">
+    <div class="b--lang-a" data-dropdown-languages v-if="languages">
         <button class="b--lang-a__btn" aria-label="Languages" data-opening-action  @click.prevent="openDropdown()">
             <svg class="b--lang-a__btn__artwork" width="25" height="26" viewBox="0 0 25 26" fill="none" xmlns="http://www.w3.org/2000/svg">
                 <path d="M16.849 23.938C18.0395 22.6487 18.6693 20.9395 18.6 19.186H1V1H24.13V15.718L24.125 15.724C24.125 15.771 24.125 15.818 24.125 15.866C24.155 20.528 17.456 24.738 16.425 24.738C16.171 24.738 16.254 24.49 16.849 23.938Z" stroke="#006EB5" stroke-width="1.5"/>
@@ -10,20 +10,19 @@
                 <path d="M11.0771 13.7918C11.0771 13.7918 6.63075 13.315 5.46875 9.36597" stroke="#006EB5" stroke-width="1.5" stroke-linecap="round"/>
                 <path d="M7.65723 4.33564V6.81318" stroke="#006EB5" stroke-width="1.5" stroke-linecap="round"/>
             </svg>
-            <span class="b--lang-a__btn__title">{{languageSelectorActive}}</span>
             <svg class="b--lang-a__btn__icon" width="17" height="12" viewBox="0 0 17 12" fill="none" xmlns="http://www.w3.org/2000/svg">
                 <path d="M1.74854 1.443L8.60205 9.87269L15.4565 1.443" stroke="#D12800" stroke-width="2"/>
             </svg>
         </button>
         <div class="b--lang-a__wrapper" data-options>
             <ul class="b--lang-a__wrapper__list-group" data-options>
-                <li class="b--lang-a__wrapper__list-group__list-item" v-if="languageSelectorActive != languages.en">
+                <li class="b--lang-a__wrapper__list-group__list-item" :class="{'b--lang-a__wrapper__list-group__list-item--is-active' : languageSelectorActive == languages.en}">
                     <a class="b--lang-a__wrapper__list-group__list-item__link" href="#" @click.prevent="changeLanguage('')">{{languages.en}}</a>
                 </li>
-                <li class="b--lang-a__wrapper__list-group__list-item" v-if="languageSelectorActive != languages.fr">
+                <li class="b--lang-a__wrapper__list-group__list-item" :class="{'b--lang-a__wrapper__list-group__list-item--is-active' : languageSelectorActive == languages.fr}">
                     <a class="b--lang-a__wrapper__list-group__list-item__link" href="#" @click.prevent="changeLanguage('fr')">{{languages.fr}}</a>
                 </li>
-                <li class="b--lang-a__wrapper__list-group__list-item" v-if="languageSelectorActive != languages.es">
+                <li class="b--lang-a__wrapper__list-group__list-item" :class="{'b--lang-a__wrapper__list-group__list-item--is-active' : languageSelectorActive == languages.es}">
                     <a class="b--lang-a__wrapper__list-group__list-item__link" href="#" @click.prevent="changeLanguage('es')">{{languages.es}}</a>
                 </li>
             </ul>
@@ -35,8 +34,7 @@
 export default {
     data:()=>{
 		return{
-            languages : null,
-            langIsVisible : true
+            languages : null
 		}
 	},
     methods : {
@@ -66,19 +64,10 @@ export default {
         },
         openDropdown() {
             document.querySelector(".b--lang-a").classList.toggle("b--lang-a--is-active");
-        },
-        handleScroll (event) {
-            // Any code to be executed when the window is scrolled
-            if(window.pageYOffset > 80){
-                this.langIsVisible = false;
-            }else{
-                this.langIsVisible = true;
-            }
         }
     },
     created(){
         if(process.client){
-            window.addEventListener('scroll', this.handleScroll);
             this.getContent();
         }
     }
