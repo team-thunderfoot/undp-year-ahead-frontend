fa235ba227db33f8da9364ddfa31e49a34b534ac
Nerea Fontecha <nerea@landt.co>
2022-01-04 12:15:32 +0100

    Story

--- components/Story.vue
+++ components/Story.vue
@@ -37,7 +37,7 @@ import Chapter14 from '~/components/Chapter14.vue';
 import Chapter15 from '~/components/Chapter15.vue';
 
 import Progress from '~/components/NavControls.vue';
-import Resize from '@/custom/custom_tf.js'
+import CustomTF from '@/custom/custom_tf.js';
 
 export default {
     data:()=>{
@@ -143,7 +143,7 @@ export default {
     },
     mounted(){
         if(process.client){
-            this.custom_tf = new Resize();
+            this.custom_tf = new CustomTF();
             
             this.urlWithParams = this.custom_tf.checkURL(); // checks if url has params
             this.sceneNumber = ( this.urlWithParams ) ? this.custom_tf.getURL() : false; // returns the param
@@ -159,7 +159,7 @@ export default {
             // event in ChapterX.vue
             this.$nuxt.$on('changeCurrent', (payload) => {
                 if(this.loadedNew){
-                    // SET NEW Item
+                    // Set New Item
                     this.currentItem = payload.item;
                     
                     // Change URL
@@ -167,14 +167,14 @@ export default {
                 }
             });
             
-            // refresh paeg on resize
+            // refresh page on resize
             window.onresize = (e)=> {  
                 if(!this.custom_tf.isMobile() && !this.custom_tf.isTablet()){
                     clearTimeout(this.timeOutFunctionId);
                     this.timeOutFunctionId = setTimeout(this.workAfterResizeIsDone(), 500);
                 }
             }
-            // refresh paeg on orientation change
+            // refresh page when orientation changes
             if(this.custom_tf.isMobile() || this.custom_tf.isTablet()){
                 window.addEventListener('orientationchange', this.workAfterResizeIsDone);
             }
